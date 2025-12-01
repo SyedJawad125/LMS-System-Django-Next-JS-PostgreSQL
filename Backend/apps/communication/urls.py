baa645f1588be7571c58121bb675a181c82153f8
Syed Jawad Ali <syedjawadali92@gmail.com>
2025-12-01 15:01:57 +0500

    Changes

--- Backend/apps/communication/urls.py
+++ Backend/apps/communication/urls.py
@@ -7,8 +7,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path('v1/announcements/', AnnouncementView.as_view()),
-    path('v1/events/', EventView.as_view()),
+    path('v1/announcement/', AnnouncementView.as_view()),
+    path('v1/event/', EventView.as_view()),
     path('v1/messages/', MessageView.as_view()),
     path('v1/notifications/', NotificationView.as_view()),
 ]
